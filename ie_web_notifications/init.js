a3be1c18b54c5b4a526530d98ced21753c07f6e4
Sergei Zabolotskikh <serggzz@gmail.com>
2017-04-02 13:59:33 +0200

    add properties title, body and tag to Notification object

--- ie_web_notifications/init.js
+++ ie_web_notifications/init.js
@@ -2,6 +2,27 @@
 'use strict';
 
 window.Notification = function Notification(title, options) {
+  Object.defineProperty(this, "title", {
+    __proto__: null,
+    value: title,
+    enumerable: true
+  });
+  if (options) {
+    if (typeof options.body === "string") {
+      Object.defineProperty(this, "body", {
+        __proto__: null,
+        value: options.body,
+        enumerable: true
+      });
+    }
+    if (typeof options.tag === "string") {
+      Object.defineProperty(this, "tag", {
+        __proto__: null,
+        value: options.tag,
+        enumerable: true
+      });
+    }
+  }
   native(this, title, options);
 };
 window.Notification.prototype = Object.create(native.prototype);
